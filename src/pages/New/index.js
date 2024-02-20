f524b3109cc496aacd2592e824a700335067bf25
GabrielSilva88 <gabrielmg.silva@outlook.com>
2024-02-20 19:15:03 -0300

    "Seção9: App Financeiro 103.Criando novo registro+104. Componente tipo de registro"

---
+++ src/pages/New/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { Background, SubmitButton, SubmitText, Input } from "./styles";
+import Header from "../../components/Header"
+import { Keyboard, SafeAreaView, TouchableWithoutFeedback } from "react-native";
+
+
+export default function New() {
+    const [labelInput, setLabelInput] = useState('');
+    const [valueInput, setValueInput] = useState('');
+    const [type, setType] = useState('receita');
+    return (
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <Background>
+                <Header title="Registrando" />
+
+                <SafeAreaView style={{ marginTop: 14, alignItems: 'center' }}>
+                    <Input 
+                        placeholder="Descrição desse registro" 
+                        value={labelInput}
+                        onChangeText={(text) => setLabelInput(text)}
+                    />
+
+                    <Input
+                        placeholder="Valor desejado"
+                        keyboardType="numeric"
+                        value={valueInput}
+                        onChangeText={(text) => setValueInput(text)}
+                    />
+
+                    <SubmitButton>
+                        <SubmitText>Resgistrar</SubmitText>
+                    </SubmitButton>
+                </SafeAreaView>
+            </Background>
+        </TouchableWithoutFeedback>
+    )
+}
